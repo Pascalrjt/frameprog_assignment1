03d38ba3524f55200718ab1e9ee95142ea578787
Pascal Roger J T <91580977+Pascalrjt@users.noreply.github.com>
2023-09-02 18:04:06 +0700

    Update assignment1.php

--- assignment1.php
+++ assignment1.php
@@ -23,6 +23,7 @@ class MobilePhone implements Thing {
         $this->os = $os;
     }
 
+    // Getter and setter for name
     public function setName($newName){
         if(!is_string($newName)){
             throw new InvalidArgumentException("Name must be a string");    
@@ -33,6 +34,68 @@ class MobilePhone implements Thing {
 
     public function getName(){
         return $this->name;    
+    }
+
+    // Getter and setter for brand
+    public function getBrand(){
+        return $this->brand;
+    }
+
+    public function setBrand($newBrand){
+        $this->brand = $newBrand;
+    }
+
+    // Getter and setter for price
+    public function getPrice(){
+        return $this->price;
+    }
+
+    public function setPrice($newPrice){
+        $this->price = $newPrice;
+    }
+
+    // Getter and setter for stock
+    public function getStock(){
+        return $this->stock;
+    }
+
+    public function setStock($newStock){
+        $this->stock = $newStock;
+    }
+
+    // Getter and setter for os
+    public function getOs(){
+        return $this->os;
+    }
+
+    public function setOs($newOs){
+        $this->os = $newOs;
+    }
+}
+
+class Inventory {
+    private $phones = []; // An array to store phone objects
+
+    // Add a phone to the inventory
+    public function addPhone(MobilePhone $phone){
+        $this->phones[] = $phone;
+    }
+
+    // Delete a phone from the inventory by name
+    public function deletePhoneByName($name){
+        foreach ($this->phones as $key => $phone) {
+            if ($phone->getName() === $name) {
+                unset($this->phones[$key]);
+                $this->phones = array_values($this->phones);
+                return true; // Phone deleted successfully
+            }
+        }
+        return false; // Phone with the given name not found
+    }
+
+    // Get the list of phones in the inventory
+    public function getInventory(){
+        return $this->phones;
     }
 }
 
